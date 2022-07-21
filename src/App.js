1ec7fa7e2f2ec9646e061a88f1e14e2de79eb718
iemprashant <prashantgupta9876@gmail.com>
2022-07-21 11:17:08 +0530

    added new page in side navbar and base of new pages

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import EmployeeDetails from './components/Blogs/EmployeeDetails';
 import ArtistPayment from './components/Artists/ArtistPayment';
 import Addcoursepage from './components/Artists/Addcoursepage';
 import AddEmployeeAccount from './components/Blogs/AddEmployeeAccount';
-import ViewCoursePage from './components/Artists/Courses/ViewCoursePage'
+import ViewCoursePage from './components/Artists/Courses/ViewCoursePage';
 import Edituniversity from './components/Artists/University/Edituniversity';
 import Editcourse from './components/Artists/Courses/Editcourse';
 import EditBlog from './components/Blogs/allblogs/EditBlog';
@@ -24,20 +24,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Property from './new-components/Property/Property';
 import Career from './new-components/Career/Career';
 import Blogs from './new-components/Blogs/Blogs';
-
+import FeaturedProject from './new-components/FeaturedProjects/FeaturedProject';
+import TrendingLoans from './new-components/TrendingLoans/TrendingLoans';
 export const history = createHistory();
 const App = () => {
   return (
     <Router history={history}>
       <Switch>
-        <Route path='/' exact component={LoginPage} />
+        <Route path="/" exact component={LoginPage} />
         <NavSidebar>
-          <Route path='/property' exact component={Property} />
-          <Route path='/career' exact component={Career} />
-          <Route path='/blogs' exact component={Blogs} />
-
-       
-               </NavSidebar>
+          <Route path="/property" exact component={Property} />
+          <Route path="/career" exact component={Career} />
+          <Route path="/blogs" exact component={Blogs} />
+          <Route path="/blog/add" exact component={AddBlogForm} />
+          <Route path="/featuredprojects" exact component={FeaturedProject} />
+          <Route path="/trendingloans" exact component={TrendingLoans} />
+        </NavSidebar>
       </Switch>
     </Router>
   );
--- src/new-components/NavSidebar.js
+++ src/new-components/NavSidebar.js
@@ -30,6 +30,9 @@ import useStyles from '../styles/NavSidebar';
 import LocationCityIcon from '@mui/icons-material/LocationCity';
 import ArticleIcon from '@mui/icons-material/Article';
 import WorkIcon from '@mui/icons-material/Work';
+import AccountBalanceOutlinedIcon from '@mui/icons-material/AccountBalanceOutlined';
+import MapsHomeWorkOutlinedIcon from '@mui/icons-material/MapsHomeWorkOutlined';
+
 const drawerWidth = 240;
 
 const AppBar = styled(MuiAppBar, {
@@ -99,21 +102,24 @@ const NavSidebar = (props) => {
     Cookies.remove('fanstarAdmin');
     history.push('/');
   };
+  const handleListClick = (url) => {
+    history.push(url);
+  };
 
   return (
     <ThemeProvider theme={mdTheme}>
       <Box sx={{ display: 'flex' }}>
         <CssBaseline />
-        <AppBar position='absolute' open={open}>
+        <AppBar position="absolute" open={open}>
           <Toolbar
             sx={{
               pr: '24px', // keep right padding when drawer closed
             }}
           >
             <IconButton
-              edge='start'
-              color='inherit'
-              aria-label='open drawer'
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
               onClick={toggleDrawer}
               sx={{
                 marginRight: '36px',
@@ -123,9 +129,9 @@ const NavSidebar = (props) => {
               <MenuIcon />
             </IconButton>
             <Typography
-              component='h1'
-              variant='h6'
-              color='inherit'
+              component="h1"
+              variant="h6"
+              color="inherit"
               noWrap
               sx={{ flexGrow: 1 }}
             >
@@ -138,7 +144,7 @@ const NavSidebar = (props) => {
             </div>
           </Toolbar>
         </AppBar>
-        <Drawer variant='permanent' open={open}>
+        <Drawer variant="permanent" open={open}>
           <Toolbar
             sx={{
               display: 'flex',
@@ -156,7 +162,6 @@ const NavSidebar = (props) => {
           </Toolbar>
           <Divider />
           <List className={classes.listDiv}>
-          
             <ListItem
               button
               className={
@@ -164,16 +169,20 @@ const NavSidebar = (props) => {
                   ? classes.selectedList
                   : ''
               }
-              onClick={() => history.push('/property')}
+              onClick={() => handleListClick('/property')}
             >
               <ListItemIcon>
                 {props.location.pathname.includes('/property') ? (
-                  <LocationCityIcon style={{color:"white",fontSize:"1.8rem"}}/>
+                  <LocationCityIcon
+                    style={{ color: 'white', fontSize: '1.8rem' }}
+                  />
                 ) : (
-                  <LocationCityIcon style={{color:"white",fontSize:"1.8rem"}}/>
+                  <LocationCityIcon
+                    style={{ color: 'white', fontSize: '1.8rem' }}
+                  />
                 )}
               </ListItemIcon>
-              <ListItemText primary='Property' />
+              <ListItemText primary="Property" />
             </ListItem>
             <ListItem
               button
@@ -182,17 +191,16 @@ const NavSidebar = (props) => {
                   ? classes.selectedList
                   : ''
               }
-              onClick={() => history.push('/blogs')}
+              onClick={() => handleListClick('/blogs')}
             >
               <ListItemIcon>
                 {props.location.pathname.includes('/blogs') ? (
-                  <ArticleIcon style={{color:"white",fontSize:"1.8rem"}}/>
-
+                  <ArticleIcon style={{ color: 'white', fontSize: '1.8rem' }} />
                 ) : (
-                  <ArticleIcon style={{color:"white",fontSize:"1.8rem"}}/>
+                  <ArticleIcon style={{ color: 'white', fontSize: '1.8rem' }} />
                 )}
               </ListItemIcon>
-              <ListItemText primary='Blogs' />
+              <ListItemText primary="Blogs" />
             </ListItem>
             <ListItem
               button
@@ -201,23 +209,65 @@ const NavSidebar = (props) => {
                   ? classes.selectedList
                   : ''
               }
-              onClick={() => history.push('/career')}
+              onClick={() => handleListClick('/career')}
             >
               <ListItemIcon>
                 {props.location.pathname.includes('/career') ? (
-                  <WorkIcon style={{color:"white",fontSize:"1.8rem"}}/>
-
+                  <WorkIcon style={{ color: 'white', fontSize: '1.8rem' }} />
                 ) : (
-                  <WorkIcon style={{color:"white",fontSize:"1.8rem"}}/>
+                  <WorkIcon style={{ color: 'white', fontSize: '1.8rem' }} />
                 )}
               </ListItemIcon>
-              <ListItemText primary='Career' />
+              <ListItemText primary="Career" />
             </ListItem>
-
+            <ListItem
+              button
+              className={
+                props.location.pathname.includes('/featuredprojects')
+                  ? classes.selectedList
+                  : ''
+              }
+              onClick={() => handleListClick('/featuredprojects')}
+            >
+              <ListItemIcon>
+                {props.location.pathname.includes('/featuredprojects') ? (
+                  <MapsHomeWorkOutlinedIcon
+                    style={{ color: 'white', fontSize: '1.8rem' }}
+                  />
+                ) : (
+                  <MapsHomeWorkOutlinedIcon
+                    style={{ color: 'white', fontSize: '1.8rem' }}
+                  />
+                )}
+              </ListItemIcon>
+              <ListItemText primary="Featured Projects" />
+            </ListItem>
+            <ListItem
+              button
+              className={
+                props.location.pathname.includes('/trendingloans')
+                  ? classes.selectedList
+                  : ''
+              }
+              onClick={() => handleListClick('/trendingloans')}
+            >
+              <ListItemIcon>
+                {props.location.pathname.includes('/trendingloans') ? (
+                  <AccountBalanceOutlinedIcon
+                    style={{ color: 'white', fontSize: '1.8rem' }}
+                  />
+                ) : (
+                  <AccountBalanceOutlinedIcon
+                    style={{ color: 'white', fontSize: '1.8rem' }}
+                  />
+                )}
+              </ListItemIcon>
+              <ListItemText primary="Trending Loans" />
+            </ListItem>
           </List>
         </Drawer>
         <Box
-          component='main'
+          component="main"
           sx={{
             backgroundColor: (theme) =>
               props.location.pathname.includes('/add')
